f01b80bc85a4b69226482208a66638df4378f1d1
dgirdyuk <dgirdyuk@intermedia.net>
2023-05-15 16:49:16 +0300

    Change mail reporter

--- mail_reporter.py
+++ mail_reporter.py
@@ -1,9 +1,21 @@
-import os
+from socket import gethostname
+from smtplib import SMTP
+from email.message import EmailMessage
 
+TO = "dgirdyuk@intermedia.com"
+SUBJECT = "Python test email"
+BODY = "test message"
 
-def send_report(to, subject, body):
-    command = "echo \"{body}\" | mail -s \"{subject}\" -aFrom:\<qa_auto_test@intermedia.net\> {to}".format(to=to, subject=subject, body=body)
-    os.system(command)
+def send_report(to, subject, body, relay="localhost"):
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg['Subject'] = subject
+    msg['From'] = gethostname() + '@intermedia.net'
+    msg['To'] = to
+    s = SMTP(relay)
+    s.send_message(msg)
+    s.quit()
+
 
 if __name__ == "__main__":
-    send_report(to="dgirdyuk@intermedia.net", subject="test", body="test")
+    send_report(TO, SUBJECT, BODY)
