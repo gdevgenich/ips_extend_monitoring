5ad345479c0dedebff40be65c34042ddcf3962cc
dgirdyuk <dgirdyuk@intermedia.net>
2023-08-06 23:25:47 +0300

    add handling of lost connection during test extend logging

--- makeall.sh
+++ makeall.sh
@@ -36,93 +36,31 @@ ls|grep "${DEB_ARCH}.deb"|while read l
   rm $l;
 done
 
-fpm   --name "$DEB_PACKAGE_NAME" \
+fpm   --name "$PACKAGE_NAME" \
       --version "$DEB_BUILD_NUMBER" \
       --architecture "${DEB_ARCH}" \
       --maintainer "${DEVEMAIL}" \
-      --depends "${PYTHON_BINARY}-audio-functions" \
-      --depends "${PYTHON_BINARY}-context" \
-      --depends "${PYTHON_BINARY}-graphyte" \
-      --depends "${PYTHON_BINARY}-monitoring-cf" \
-      --depends "${PYTHON_BINARY}-pynetwork" \
-      --depends "${PYTHON_BINARY}-sipde-client" \
-      --depends "${PYTHON_BINARY}-siplib" \
-      --depends "${PYTHON_BINARY}-reactor" \
-      --depends "${PYTHON_BINARY}-step-manager" \
-      --depends "${PYTHON_BINARY}-configparser2" \
-      --depends "${PYTHON_BINARY}-pytelegraf" \
-      --depends "${PYTHON_BINARY}-anymeeting-client = 0.2" \
       --depends "${PYTHON_BINARY}-audio-functions = 2.1" \
-      --depends "${PYTHON_BINARY}-base-callfunc = 1.2" \
-      --depends "${PYTHON_BINARY}-blf-parser = 1.2" \
-      --depends "${PYTHON_BINARY}-call-functions = 7.20" \
-      --depends "${PYTHON_BINARY}-configparser2 = 2.2" \
-      --depends "${PYTHON_BINARY}-context = 1.4" \
-      --depends "${PYTHON_BINARY}-future-comparators = 1.0.1" \
-      --depends "${PYTHON_BINARY}-hpbx-base = 2.83" \
-      --depends "${PYTHON_BINARY}-hpbx-dm = 7.78" \
-      --depends "${PYTHON_BINARY}-ips-client = 5.11" \
-      --depends "${PYTHON_BINARY}-ips-control = 1.32" \
-      --depends "${PYTHON_BINARY}-log-functions = 1.0" \
-      --depends "${PYTHON_BINARY}-mailbox-functions = 2.6" \
-      --depends "${PYTHON_BINARY}-mailcc = 2.0" \
-      --depends "${PYTHON_BINARY}-msg-util = 1.0.0" \
-      --depends "${PYTHON_BINARY}-mwi-parser = 1.0" \
-      --depends "${PYTHON_BINARY}-pbxut = 4.5.0" \
-      --depends "${PYTHON_BINARY}-pbxut-plugin-alert-case = 1.0" \
-      --depends "${PYTHON_BINARY}-pbxut-plugin-classic-reporter = 1.0.0" \
-      --depends "${PYTHON_BINARY}-pbxut-plugin-context = 1.0.0" \
-      --depends "${PYTHON_BINARY}-pbxut-plugin-context-reader = 1.0.2" \
-      --depends "${PYTHON_BINARY}-pbxut-plugin-hpbx-version = 1.0.0" \
-      --depends "${PYTHON_BINARY}-pbxut-plugin-mail-reporter = 1.1.0" \
-      --depends "${PYTHON_BINARY}-pbxut-plugin-simple-filter = 1.0.0" \
-      --depends "${PYTHON_BINARY}-pbxut-plugin-summary-case = 1.0.0" \
-      --depends "${PYTHON_BINARY}-pbxut-plugin-testlink = 1.0.0" \
-      --depends "${PYTHON_BINARY}-pbxut-plugin-verbose-reporter = 1.0.1" \
-      --depends "${PYTHON_BINARY}-pbxut-util = 4.5.0" \
-      --depends "${PYTHON_BINARY}-pynetwork = 2.2" \
+      --depends "${PYTHON_BINARY}-configparser2 >= 2.2" \
+      --depends "${PYTHON_BINARY}-context >= 1.4" \
+      --depends "${PYTHON_BINARY}-graphyte >= 1.7.1" \
+      --depends "${PYTHON_BINARY}-monitoring-cf = 1.9" \
+      --depends "${PYTHON_BINARY}-pynetwork >= 2.3" \
       --depends "${PYTHON_BINARY}-reactor = 3.0" \
-      --depends "${PYTHON_BINARY}-rmq-client = 1.8" \
-      --depends "${PYTHON_BINARY}-robert2 = 2.0" \
-      --depends "${PYTHON_BINARY}-rpclient = 1.2" \
-      --depends "${PYTHON_BINARY}-sdplib = 1.0" \
-      --depends "${PYTHON_BINARY}-sipde-client = 2.4.1" \
+      --depends "${PYTHON_BINARY}-sipde-client = 2.4.4" \
       --depends "${PYTHON_BINARY}-siplib = 4.0" \
-      --depends "${PYTHON_BINARY}-sipne = 3.5.1" \
-      --depends "${PYTHON_BINARY}-soap-client = 1.1" \
       --depends "${PYTHON_BINARY}-step-manager = 2.12" \
-      --depends "${PYTHON_BINARY}-testlink = 3.0" \
-      --depends "${PYTHON_BINARY}-umsctl-client = 1.10" \
-      --depends "${PYTHON_BINARY}-unison-cp-client = 1.22" \
-      --depends "${PYTHON_BINARY}-protobuf = 3.17.3" \
-      --depends "${PYTHON_BINARY}-pytz" \
-      --depends "${PYTHON_BINARY}-waveproc" \
-      --depends "${PYTHON_BINARY}-multipart" \
-      --depends "${PYTHON_BINARY}-signalrcore" \
-      --depends "python3" \
-      --depends "python3-requests" \
-      --depends "python3-urllib3" \
-      --depends "python3-chardet" \
-      --depends "python3-certifi" \
-      --depends "python3-idna" \
-      --depends "python3-slixmpp" \
-      --depends "python3-magic" \
+      --depends "${PYTHON_BINARY}-waveproc = 2.11" \
+      --depends "${PYTHON_BINARY}-rmq-client = 2.5" \
+      --depends "${PYTHON_BINARY}-pika = 1.3.1" \
+      --depends "sipmon = 2.10.0-1+deb11" \
+      --depends "nodejs = 16.18.1-deb-1nodesource1" \
       --depends "sipde = 1.3.0-1+deb11" \
-      --depends "sipmon = 2.7.1-1+deb11" \
-      --depends "libpjproject = 2.11-4+deb11" \
-      --depends "intermedia-ring = 0.0.2" \
-      --depends "nodejs = 12.22.12~dfsg-1~deb11u1" \
-      --depends ffmpeg \
-      --depends sipmon \
-      --depends sipde \
-      --depends libpjproject \
-      --depends mailutils \
-      --depends xvfb \
-      --depends libxss1 \
-      --depends xdg-utils \
-      --depends libasound2 \
+      --depends "libpjproject = 2.13-5+deb11" \
+      --depends "ffmpeg >= 7:4.3.4-0+deb11u1" \
+      --depends "mailutils = 1:3.10-3+b1" \
       --deb-user $DEB_USER \
-      --deb-group $DEB_USER \
-      --description "${DEB_PACKAGE_DESC}" \
+      --deb-group $DEB_GROUP \
+      --description "${PACKAGE_DESC}" \
       --deb-no-default-config-files \
       -t deb -s dir opt/ var/
