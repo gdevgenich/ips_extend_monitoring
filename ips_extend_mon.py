2f9e20a843acc84d9d200c70377fb2b0921470b2
dgirdyuk <dgirdyuk@intermedia.net>
2024-07-19 17:09:47 +0300

    Improve logging

--- ips_extend_mon.py
+++ ips_extend_mon.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
 
     try:
         ips_extend_checker.read_data_from_config()
-        ips_extend_checker.clients_init()
         ips_extend_checker.set_logging()
+        ips_extend_checker.clients_init()
         ips_extend_checker.check()
     except Exception as e:
         ips_extend_checker.error_message = str(e)
